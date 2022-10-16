15eff7c44b225a277015f9ccb5c3b2e3db79ef02
GuanRu Yu <aprilyu@MSI.localdomain>
2022-10-15 19:42:43 -0700

    done

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -4,15 +4,14 @@ public protocol WeatherService {
     func getTemperature() async throws -> Int
 }
 
-/*
+
 enum BaseUrl: String {
     case openweathermap = "https://api.openweathermap.org"
-    case mockServer = "http://localhost:3000/data/2.5/weather"
+    case mockServer = "http://localhost:3000"
 }
-*/
-class WeatherServiceImpl: WeatherService {
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=4d063a9f4c350cebe7ca77110ffc03d5"
 
+class WeatherServiceImpl: WeatherService {
+    let url = "\(BaseUrl.openweathermap.rawValue)/data/2.5/weather?q=corvallis&units=imperial&appid=4d063a9f4c350cebe7ca77110ffc03d5"
     func getTemperature() async throws -> Int {
         return try await withCheckedThrowingContinuation { continuation in
             AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -71,67 +71,22 @@ final class MyLibraryTests: XCTestCase {
 
     //myunittest
     func testWeatherModel() async {
-
-        //let myTemp = Weather.self()
-
-        //XCTAssertNil(myTemp)
+        // Given
 
         let jasonString = """
         {
-            "coord": {
-                "lon": 10.99,
-                "lat": 44.34
-            },
-            "weather": [
-                {
-                "id": 501,
-                "main": "Rain",
-                "description": "moderate rain",
-                "icon": "10d"
-                }
-            ],
-            "base": "stations",
             "main": {
-                "temp": 298.48,
-                "feels_like": 298.74,
-                "temp_min": 297.56,
-                "temp_max": 300.05,
-                "pressure": 1015,
-                "humidity": 64,
-                "sea_level": 1015,
-                "grnd_level": 933
-            },
-            "visibility": 10000,
-            "wind": {
-                "speed": 0.62,
-                "deg": 349,
-                "gust": 1.18
-            },
-            "rain": {
-                "1h": 3.16
-            },
-            "clouds": {
-                "all": 100
-            },
-            "dt": 1661870592,
-            "sys": {
-                "type": 2,
-                "id": 2075663,
-                "country": "IT",
-                "sunrise": 1661834187,
-                "sunset": 1661882248
-            },
-            "timezone": 7200,
-            "id": 3163858,
-            "name": "Zocca",
-            "cod": 200
+                "temp": 293.64,
+            }
         }
         """
         let data = Data(jasonString.utf8)
         
         do{
+            // When
             let jsonData = try JSONDecoder().decode(Weather.self, from: data)
-            XCTAssertEqual(jsonData.main.temp, 298.48)
+            // Then
+            XCTAssertEqual(jsonData.main.temp, 293.64)
         }catch{
             XCTAssert(false)
         }
@@ -139,19 +94,18 @@ final class MyLibraryTests: XCTestCase {
         
     }
 
-/*
+
     //my integration test
-    func testIsNotLuckyBecauseServiceCallFails123() async {
-        // Given
-        
-        let myLibrary = MyLibrary(weatherService: nil)
+    func testIntegrationTest() async throws{
+        //Given
+        let weatherService = WeatherServiceImpl()
+        //When
+        let myTemp = try await weatherService.getTemperature()
+        //Then
+        XCTAssertNotNil(myTemp)
 
-        // When
-        let isLuckyNumber = await myLibrary.isLucky(7)
-
-        // Then
-        XCTAssertNotNil(isLuckyNumber)
+        XCTAssertEqual(myTemp, 80)
     }
 
-*/
+
 }
